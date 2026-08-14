1b531a30bd793c0aa15bb042fbc885d214170710
wafadarhussain1214 <wafadar.ghazi@kabirtechsolutions.com>
2026-08-14 14:44:34 +0500

    create the Todo List task

---
+++ my-react-app/src/components/Todo/TodoForm.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+
+function TodoForm({ onAddTodo }) {
+
+  const [todoText, setTodoText] = useState("");
+
+  const handleSubmit = (event) => {
+
+    event.preventDefault();
+
+    const text = todoText.trim();
+
+    if (!text) {
+      return;
+    }
+
+    onAddTodo(text);
+
+    setTodoText("");
+  };
+
+  return (
+    <form className="todo-form" onSubmit={handleSubmit}>
+
+      <input type="text"  placeholder="Enter a todo..." value={todoText}
+        onChange={(event) =>
+          setTodoText(event.target.value)
+        }
+      />
+
+      <button type="submit">
+        Add Todo
+      </button>
+
+    </form>
+  );
+}
+
+export default TodoForm;
---
+++ my-react-app/src/pages/ProfilePage.jsx
@@ -0,0 +1,27 @@
+import ProfileCard from '../components/ProfileCard'
+import profiles from '../data/profiles'
+
+function ProfilePage(){
+    return(
+        <div className="profile-page">
+            <h1>Our Team</h1>
+            <div className="profile-list">
+                {   
+                    profiles.map((profile)=>(
+                        <ProfileCard 
+                            key={profile.id}
+                            name={profile.name}
+                            email={profile.email}
+                            role={profile.role}
+                            image={profile.image}
+                            skills={profile.skills}
+                        />
+                    ))
+                }
+            </div>
+        </div>
+    )
+}
+
+
+export default ProfilePage
